e8473972c4312c8966ed6439217ee139328f3f9b
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-04 21:52:07 +0900

    indexとnewページの基本実装

--- app/controllers/voices_controller.rb
+++ app/controllers/voices_controller.rb
@@ -1,4 +1,16 @@
 class VoicesController < ApplicationController
   def index
+    @voice = Voice.all
+  end
+  def new
+    @voice = Voice.new
+  end
+  def create
+    Voice.create(voice_params)
+    redirect_to new_voice_path
+  end
+  private
+  def voice_params
+    params.require(:voice).permit(:content)
   end
 end
